f3ab93cda37fdcd67d262fccc7b1dc382c5e8102
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-09 01:15:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   #your code here
-  attr_accessor :name
+  attr_accessor :name, :hair_color, :birthday
   def initialize(args)
     args.each { |key, value| self.send("#{key}=",value)  }
 
