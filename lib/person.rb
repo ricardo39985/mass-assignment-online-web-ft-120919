a4cc3925893ac5c72e9127f59bd3417bdc37f01b
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-09 01:13:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   #your code here
   def initialize(args)
-    args.each { |key, value| self.send("#{key}="value)  }
+    args.each { |key, value| self.send("#{key}=",value)  }
 
   end
 end
